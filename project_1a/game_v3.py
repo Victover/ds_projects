08971e92a6126ed7bc993120902c9c011d97bb81
Victover <virol8@mail.ru>
2022-06-20 11:21:58 +0300

    edit1

--- project_1a/game_v3.py
+++ project_1a/game_v3.py
@@ -16,12 +16,12 @@ def random_predict(number:int=np.random.randint(1, 101)) -> int:
     """
 
     count = 0
-    lst_num = list(range(1,101))
+    lst_num = list(range(1, 101))
 
     while True:
         count += 1
         predict_number = int(np.mean(lst_num))
-        half = round((len(lst_num))/2)
+        half = round((len(lst_num)) / 2)
         if number == predict_number:
             break # выход из цикла, если угадали
         elif predict_number > number:
